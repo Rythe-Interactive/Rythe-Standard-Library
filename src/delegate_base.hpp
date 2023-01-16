92da95b46938362394083ad0515835565bb4f035
Glyn Leine <g.m.leine@gmail.com>
2023-01-16 18:57:42 +0100

    add extensionless files

--- src/delegate_base.hpp
+++ src/delegate_base.hpp
@@ -1,48 +1,48 @@
 /*
 
-	Copyright (C) 2017 by Sergey A Kryukov: derived work
-	http://www.SAKryukov.org
-	http://www.codeproject.com/Members/SAKryukov
+    Copyright (C) 2017 by Sergey A Kryukov: derived work
+    http://www.SAKryukov.org
+    http://www.codeproject.com/Members/SAKryukov
 
-	Based on original work by Sergey Ryazanov:
-	"The Impossibly Fast C++ Delegates", 18 Jul 2005
-	https://www.codeproject.com/articles/11015/the-impossibly-fast-c-delegates
+    Based on original work by Sergey Ryazanov:
+    "The Impossibly Fast C++ Delegates", 18 Jul 2005
+    https://www.codeproject.com/articles/11015/the-impossibly-fast-c-delegates
 
-	MIT license:
-	http://en.wikipedia.org/wiki/MIT_License
+    MIT license:
+    http://en.wikipedia.org/wiki/MIT_License
 
-	Original publication: https://www.codeproject.com/Articles/1170503/The-Impossibly-Fast-Cplusplus-Delegates-Fixed
+    Original publication: https://www.codeproject.com/Articles/1170503/The-Impossibly-Fast-Cplusplus-Delegates-Fixed
 
 */
 
 #pragma once
 
 namespace rythe {
-	
-	template<typename T>
-	class delegate_base;
-	
-	template<typename RET, typename ...PARAMS>
-	class delegate_base<RET(PARAMS...)>	{
-	protected:
-		using stub_type = RET(*)(void* this_ptr, PARAMS...);
+    
+    template<typename T>
+    class delegate_base;
+    
+    template<typename RET, typename ...PARAMS>
+    class delegate_base<RET(PARAMS...)>	{
+    protected:
+        using stub_type = RET(*)(void* this_ptr, PARAMS...);
 
-		struct invocation_element {
-			invocation_element() = default;
-			invocation_element(void* object, stub_type stub) : m_object(object), m_stub(stub) {}
-			invocation_element(const invocation_element& other) m_object(other.m_object), m_stub(other.m_stub) {}
+        struct invocation_element {
+            invocation_element() = default;
+            invocation_element(void* object, stub_type stub) : m_object(object), m_stub(stub) {}
+            invocation_element(const invocation_element& other) m_object(other.m_object), m_stub(other.m_stub) {}
 
-			bool operator ==(const invocation_element& other) const {
-				return other.m_stub == m_stub && other.m_object == m_object;
-			}
+            bool operator ==(const invocation_element& other) const {
+                return other.m_stub == m_stub && other.m_object == m_object;
+            }
 
-			bool operator !=(const invocation_element& other) const {
-				return other.m_stub != m_stub || other.m_object != m_object;
-			}
+            bool operator !=(const invocation_element& other) const {
+                return other.m_stub != m_stub || other.m_object != m_object;
+            }
 
-			void* m_object = nullptr;
-			stub_type m_stub = nullptr;
-		};
-	};
+            void* m_object = nullptr;
+            stub_type m_stub = nullptr;
+        };
+    };
 
 }
--- src/multicast_delegate.hpp
+++ src/multicast_delegate.hpp
@@ -1,17 +1,17 @@
 /*
 
-	Copyright (C) 2017 by Sergey A Kryukov: derived work
-	http://www.SAKryukov.org
-	http://www.codeproject.com/Members/SAKryukov
+    Copyright (C) 2017 by Sergey A Kryukov: derived work
+    http://www.SAKryukov.org
+    http://www.codeproject.com/Members/SAKryukov
 
-	Based on original work by Sergey Ryazanov:
-	"The Impossibly Fast C++ Delegates", 18 Jul 2005
-	https://www.codeproject.com/articles/11015/the-impossibly-fast-c-delegates
+    Based on original work by Sergey Ryazanov:
+    "The Impossibly Fast C++ Delegates", 18 Jul 2005
+    https://www.codeproject.com/articles/11015/the-impossibly-fast-c-delegates
 
-	MIT license:
-	http://en.wikipedia.org/wiki/MIT_License
+    MIT license:
+    http://en.wikipedia.org/wiki/MIT_License
 
-	Original publication: https://www.codeproject.com/Articles/1170503/The-Impossibly-Fast-Cplusplus-Delegates-Fixed
+    Original publication: https://www.codeproject.com/Articles/1170503/The-Impossibly-Fast-Cplusplus-Delegates-Fixed
 
 */
 
@@ -22,99 +22,99 @@
 
 namespace rythe {
 
-	template<typename RET, typename ...PARAMS>
-	class multicast_delegate<RET(PARAMS...)> final : private delegate_base<RET(PARAMS...)> {
-	public:
+    template<typename RET, typename ...PARAMS>
+    class multicast_delegate<RET(PARAMS...)> final : private delegate_base<RET(PARAMS...)> {
+    public:
 
         using ReturnType = RET;
         using ParamTypes = PARAMS;
 
-		multicast_delegate() = default;
-		~multicast_delegate() {
-			for (auto& element : m_invocationList) delete element;
-			m_invocationList.clear();
-		}
+        multicast_delegate() = default;
+        ~multicast_delegate() {
+            for (auto& element : m_invocationList) delete element;
+            m_invocationList.clear();
+        }
 
-		bool empty() const { return m_invocationList.empty(); }
-		bool operator ==(std::nullptr_t) const { return empty(); }
+        bool empty() const { return m_invocationList.empty(); }
+        bool operator ==(std::nullptr_t) const { return empty(); }
         bool operator !=(std::nullptr_t) const { return !empty(); }
         operator bool() const { return !empty(); }
 
-		size_t size() const { return m_invocationList.size(); }
+        size_t size() const { return m_invocationList.size(); }
 
-		multicast_delegate& operator =(const multicast_delegate&) = delete;
-		multicast_delegate(const multicast_delegate&) = delete;
+        multicast_delegate& operator =(const multicast_delegate&) = delete;
+        multicast_delegate(const multicast_delegate&) = delete;
 
-		bool operator ==(const multicast_delegate& another) const {
-			if (m_invocationList.size() != another.m_invocationList.size())
+        bool operator ==(const multicast_delegate& another) const {
+            if (m_invocationList.size() != another.m_invocationList.size())
                 return false;
 
-			auto anotherIt = another.m_invocationList.begin();
-			for (auto it = m_invocationList.begin(); it != m_invocationList.end(); ++it)
-				if (**it != **anotherIt)
+            auto anotherIt = another.m_invocationList.begin();
+            for (auto it = m_invocationList.begin(); it != m_invocationList.end(); ++it)
+                if (**it != **anotherIt)
                     return false;
 
-			return true;
-		}
+            return true;
+        }
 
-		bool operator !=(const multicast_delegate& another) const { return !(*this == another); }
+        bool operator !=(const multicast_delegate& another) const { return !(*this == another); }
 
-		bool operator ==(const delegate<ReturnType(ParamTypes...)>& another) const {
-			if (isNull() && another.isNull()) return true;
-			if (another.isNull() || (size() != 1)) return false;
-			return (another.invocation == **m_invocationList.begin());
-		}
+        bool operator ==(const delegate<ReturnType(ParamTypes...)>& another) const {
+            if (isNull() && another.isNull()) return true;
+            if (another.isNull() || (size() != 1)) return false;
+            return (another.invocation == **m_invocationList.begin());
+        }
 
-		bool operator !=(const delegate<ReturnType(ParamTypes...)>& another) const { return !(*this == another); }
+        bool operator !=(const delegate<ReturnType(ParamTypes...)>& another) const { return !(*this == another); }
 
-		multicast_delegate& operator +=(const multicast_delegate& another) {
-			for (auto& item : another.m_invocationList) // clone, not copy; flattens hierarchy:
-				this->m_invocationList.push_back(new typename delegate_base<ReturnType(ParamTypes...)>::invocation_element(item->object, item->stub));
-			return *this;
-		}
+        multicast_delegate& operator +=(const multicast_delegate& another) {
+            for (auto& item : another.m_invocationList) // clone, not copy; flattens hierarchy:
+                this->m_invocationList.push_back(new typename delegate_base<ReturnType(ParamTypes...)>::invocation_element(item->object, item->stub));
+            return *this;
+        }
 
-		template <typename LAMBDA> // template instantiation is not neededm, will be deduced/inferred:
-		multicast_delegate& operator +=(const LAMBDA & lambda) {
-			delegate<ReturnType(ParamTypes...)> d = delegate<ReturnType(ParamTypes...)>::template create<LAMBDA>(lambda);
-			return *this += d;
-		}
+        template <typename LAMBDA> // template instantiation is not neededm, will be deduced/inferred:
+        multicast_delegate& operator +=(const LAMBDA & lambda) {
+            delegate<ReturnType(ParamTypes...)> d = delegate<ReturnType(ParamTypes...)>::template create<LAMBDA>(lambda);
+            return *this += d;
+        }
 
-		multicast_delegate& operator +=(const delegate<ReturnType(ParamTypes...)>& another) {
-			if (another.isNull()) return *this;
-			this->m_invocationList.push_back(new typename delegate_base<ReturnType(ParamTypes...)>::invocation_element(another.invocation.object, another.invocation.stub));
-			return *this;
-		}
+        multicast_delegate& operator +=(const delegate<ReturnType(ParamTypes...)>& another) {
+            if (another.isNull()) return *this;
+            this->m_invocationList.push_back(new typename delegate_base<ReturnType(ParamTypes...)>::invocation_element(another.invocation.object, another.invocation.stub));
+            return *this;
+        }
 
-		// will work even if ReturnType is void, return values are ignored:
-		// (for handling return values, see operator(..., handler))
-		void operator()(ParamTypes... arg) const {
-			for (auto& item : m_invocationList)
-				(*(item->stub))(item->object, arg...);
-		}
+        // will work even if ReturnType is void, return values are ignored:
+        // (for handling return values, see operator(..., handler))
+        void operator()(ParamTypes... arg) const {
+            for (auto& item : m_invocationList)
+                (*(item->stub))(item->object, arg...);
+        }
 
-		template<typename HANDLER>
-		void operator()(ParamTypes... arg, HANDLER handler) const {
-			size_t index = 0;
-			for (auto& item : m_invocationList) {
-				ReturnType value = (*(item->stub))(item->object, arg...);
-				handler(index, &value);
-				++index;
-			}
-		}
+        template<typename HANDLER>
+        void operator()(ParamTypes... arg, HANDLER handler) const {
+            size_t index = 0;
+            for (auto& item : m_invocationList) {
+                ReturnType value = (*(item->stub))(item->object, arg...);
+                handler(index, &value);
+                ++index;
+            }
+        }
 
-		void operator()(ParamTypes... arg, delegate<void(size_t, ReturnType*)> handler) const {
-			operator()<decltype(handler)>(arg..., handler);
-		}
+        void operator()(ParamTypes... arg, delegate<void(size_t, ReturnType*)> handler) const {
+            operator()<decltype(handler)>(arg..., handler);
+        }
 
-		void operator()(ParamTypes... arg, std::function<void(size_t, ReturnType*)> handler) const {
-			operator()<decltype(handler)>(arg..., handler);
-		}
+        void operator()(ParamTypes... arg, std::function<void(size_t, ReturnType*)> handler) const {
+            operator()<decltype(handler)>(arg..., handler);
+        }
 
-	private:
+    private:
 
-		std::list<typename delegate_base<ReturnType(ParamTypes...)>::invocation_element *> m_invocationList;
+        std::list<typename delegate_base<ReturnType(ParamTypes...)>::invocation_element *> m_invocationList;
 
-	};
+    };
 
 }
 
