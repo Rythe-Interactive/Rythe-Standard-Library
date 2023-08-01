d1281473cee67155426c8a3795cd253397f3a7e3
Rowan Ramsey <blazinram@gmail.com>
2023-08-01 21:39:16 +0200

    updating the premake stuff

--- src/rsl/build-rsl.lua
+++ src/rsl/build-rsl.lua
@@ -1,30 +1,8 @@
---! Legion Core Build Script for premake5
---[[
-authors: Raphael Baier, Glyn Leine
-copyright: (c) 2020 Raphael Baier, The Legion-Team
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this
-software and associated documentation files (the "Software"), to deal in the Software
-without restriction, including without limitation the rights to use, copy, modify, merge,
-publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
-to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or
-substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
-FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
-
-]]--
-
-project "rsl"
-    kind "StaticLib"
-    language "C++"
-    cppdialect "C++20"
-    includedirs {"impl/"}
-
-    files { "**.h", "**.hpp" ,"**.inl","**.c", "**.cpp" }
+-- group "include"
+-- project "rsl"
+--     location "src/rsl/"
+--     kind "StaticLib"
+createProject("include","rsl","StaticLib")
+includedirs {"src/impl/"}
+files { "src/impl/**.h", "src/impl/**.hpp" ,"src/impl/**.inl","src/impl/**.c", "src/impl/**.cpp" }
+--group ""
