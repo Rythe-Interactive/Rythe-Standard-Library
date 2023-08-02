aa26b907d863b5d2fde85ac8da227d5ba14ac022
Rowan Ramsey <blazinram@gmail.com>
2023-08-02 04:52:48 +0200

    updated the way we build rsl in premake

--- build-rsl.lua
+++ build-rsl.lua
@@ -1,51 +1,20 @@
---! Legion Core Build Script for premake5
---[[
-authors: Raphael Baier, Glyn Leine
-copyright: (c) 2020 Raphael Baier, The Legion-Team
+createProject ("include","rsl","StaticLib")
+includedirs {
+    "$(SolutionDir)include/rsl/src/"
+}
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this
-software and associated documentation files (the "Software"), to deal in the Software
-without restriction, including without limitation the rights to use, copy, modify, merge,
-publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
-to whom the Software is furnished to do so, subject to the following conditions:
+files { 
+    "src/**.h",
+    "src/**.hpp",
+    "src/**.inl",
+    "src/**.c",
+    "src/**.cpp"
+}
 
-The above copyright notice and this permission notice shall be included in all copies or
-substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
-FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
-
-]]--
-
-workspace "rsl"
-    configurations { "Debug", "Release" }
-
-include "src/rsl/build-rsl.lua"
-
-project "*"
+targetdir "build/%{cfg.buildcfg}"
+libdirs { 
+    "lib/", 
+    "build/%{cfg.buildcfg}/"
+ }
 
 
-project "rsl"
-    location "src/rsl"
-    kind "StaticLib"
-    language "C++"
-    cppdialect "C++20"
-    toolset "clang"
-    architecture "x86_64"
-
-    includedirs {"src/rsl/impl/","impl/"}
-    targetdir "build/%{cfg.buildcfg}"
-    libdirs { "lib/", "build/%{cfg.buildcfg}/" }
-    files { "**.h", "**.hpp" ,"**.inl","**.c", "**.cpp" }
-
-    filter "configurations:Debug"
-        defines {"DEBUG"}
-        symbols "On"
-
-    filter "configurations:Release"
-        defines {"NDEBUG"}
-        optimize "On"
