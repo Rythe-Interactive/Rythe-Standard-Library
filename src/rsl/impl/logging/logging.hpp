04e6bef77f5bb5239f5be99b3346af18bee3d117
Glyn Leine <g.m.leine@gmail.com>
2025-09-06 18:59:12 +0200

    move logging function definitions to inl for header readability, allow compile time stripping of trace, debug, and info logs

--- src/rsl/impl/logging/logging.hpp
+++ src/rsl/impl/logging/logging.hpp
@@ -15,94 +15,38 @@ namespace rsl
 	namespace log
     {
         template <typename... Args>
-        [[rythe_always_inline]] void println(severity s, format_string format, Args&&... args) noexcept
-        {
-            get_logging_context().logger->log(s, format, rsl::forward<Args>(args)...);
-        }
+        [[rythe_always_inline]] void println(severity s, format_string format, Args&&... args) noexcept;
+        template <typename... Args>
+        [[rythe_always_inline]] void undecoratedln(severity s, format_string format, Args&&... args) noexcept;
+
+        [[rythe_always_inline]] inline void filter(severity level);
 
         template <typename... Args>
-        [[rythe_always_inline]] void undecoratedln(severity s, format_string format, Args&&... args) noexcept
-        {
-            get_logging_context().undecoratedLogger->log(s, format, rsl::forward<Args>(args)...);
-        }
-
-        [[rythe_always_inline]] static void filter(const severity level)
-        {
-            logging_context& context = get_logging_context();
-            context.logger->filter(level);
-            context.undecoratedLogger->filter(level);
-        }
+        [[rythe_always_inline]] void trace(format_string format, Args&&... args);
+        template <typename... Args>
+        [[rythe_always_inline]] void debug(format_string format, Args&&... args);
+        template <typename... Args>
+        [[rythe_always_inline]] void info(format_string format, Args&&... args);
+        template <typename... Args>
+        [[rythe_always_inline]] void warn(format_string format, Args&&... args);
+        template <typename... Args>
+        [[rythe_always_inline]] void error(format_string format, Args&&... args);
+        template <typename... Args>
+        [[rythe_always_inline]] void fatal(format_string format, Args&&... args);
 
         template <typename... Args>
-        [[rythe_always_inline]] void undecorated_trace(format_string format, Args&&... args)
-        {
-            undecoratedln(severity::trace, format, rsl::forward<Args>(args)...);
-        }
-
+        [[rythe_always_inline]] void undecorated_trace(format_string format, Args&&... args);
         template <typename... Args>
-        [[rythe_always_inline]] void undecorated_debug(format_string format, Args&&... args)
-        {
-            undecoratedln(severity::debug, format, rsl::forward<Args>(args)...);
-        }
-
+        [[rythe_always_inline]] void undecorated_debug(format_string format, Args&&... args);
         template <typename... Args>
-        [[rythe_always_inline]] void undecorated_info(format_string format, Args&&... args)
-        {
-            undecoratedln(severity::info, format, rsl::forward<Args>(args)...);
-        }
-
+        [[rythe_always_inline]] void undecorated_info(format_string format, Args&&... args);
         template <typename... Args>
-        [[rythe_always_inline]] void undecorated_warn(format_string format, Args&&... args)
-        {
-            undecoratedln(severity::warn, format, rsl::forward<Args>(args)...);
-        }
-
+        [[rythe_always_inline]] void undecorated_warn(format_string format, Args&&... args);
         template <typename... Args>
-        [[rythe_always_inline]] void undecorated_error(format_string format, Args&&... args)
-        {
-            undecoratedln(severity::error, format, rsl::forward<Args>(args)...);
-        }
-
+        [[rythe_always_inline]] void undecorated_error(format_string format, Args&&... args);
         template <typename... Args>
-        [[rythe_always_inline]] void undecorated_fatal(format_string format, Args&&... args)
-        {
-            undecoratedln(severity::fatal, format, rsl::forward<Args>(args)...);
-        }
-
-        template <typename... Args>
-        [[rythe_always_inline]] void trace(format_string format, Args&&... args)
-        {
-            println(severity::trace, format, rsl::forward<Args>(args)...);
-        }
-
-        template <typename... Args>
-        [[rythe_always_inline]] void debug(format_string format, Args&&... args)
-        {
-            println(severity::debug, format, rsl::forward<Args>(args)...);
-        }
-
-        template <typename... Args>
-        [[rythe_always_inline]] void info(format_string format, Args&&... args)
-        {
-            println(severity::info, format, rsl::forward<Args>(args)...);
-        }
-
-        template <typename... Args>
-        [[rythe_always_inline]] void warn(format_string format, Args&&... args)
-        {
-            println(severity::warn, format, rsl::forward<Args>(args)...);
-        }
-
-        template <typename... Args>
-        [[rythe_always_inline]] void error(format_string format, Args&&... args)
-        {
-            println(severity::error, format, rsl::forward<Args>(args)...);
-        }
-
-        template <typename... Args>
-        [[rythe_always_inline]] void fatal(format_string format, Args&&... args)
-        {
-            println(severity::fatal, format, rsl::forward<Args>(args)...);
-        }
+        [[rythe_always_inline]] void undecorated_fatal(format_string format, Args&&... args);
     } // namespace log
 } // namespace rsl
+
+#include "logging.inl"
---
+++ src/rsl/impl/logging/logging.inl
@@ -0,0 +1,106 @@
+#pragma once
+namespace rsl::log
+{
+    template <typename... Args>
+    void println(const severity s, const format_string format, Args&&... args) noexcept
+    {
+        get_logging_context().logger->log(s, format, rsl::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    void undecoratedln(const severity s, const format_string format, Args&&... args) noexcept
+    {
+        get_logging_context().undecoratedLogger->log(s, format, rsl::forward<Args>(args)...);
+    }
+
+    inline void filter(const severity level)
+    {
+        logging_context& context = get_logging_context();
+        context.logger->filter(level);
+        context.undecoratedLogger->filter(level);
+    }
+
+    template <typename... Args>
+    void trace([[maybe_unused]] const format_string format, [[maybe_unused]] Args&&... args)
+    {
+        #if !defined(RSL_STRIP_TRACE_LOG)
+        println(severity::trace, format, rsl::forward<Args>(args)...);
+        #endif
+    }
+
+    template <typename... Args>
+    void debug([[maybe_unused]] const format_string format, [[maybe_unused]] Args&&... args)
+    {
+        #if !defined(RSL_STRIP_DEBUG_LOG)
+        println(severity::debug, format, rsl::forward<Args>(args)...);
+        #endif
+    }
+
+    template <typename... Args>
+    void info([[maybe_unused]] const format_string format, [[maybe_unused]] Args&&... args)
+    {
+        #if !defined(RSL_STRIP_INFO_LOG)
+        println(severity::info, format, rsl::forward<Args>(args)...);
+        #endif
+    }
+
+    template <typename... Args>
+    void warn(const format_string format, Args&&... args)
+    {
+        println(severity::warn, format, rsl::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    void error(const format_string format, Args&&... args)
+    {
+        println(severity::error, format, rsl::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    void fatal(const format_string format, Args&&... args)
+    {
+        println(severity::fatal, format, rsl::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    void undecorated_trace([[maybe_unused]] const format_string format, [[maybe_unused]] Args&&... args)
+    {
+        #if !defined(RSL_STRIP_TRACE_LOG)
+        undecoratedln(severity::trace, format, rsl::forward<Args>(args)...);
+        #endif
+    }
+
+    template <typename... Args>
+    void undecorated_debug([[maybe_unused]] const format_string format, [[maybe_unused]] Args&&... args)
+    {
+        #if !defined(RSL_STRIP_DEBUG_LOG)
+        undecoratedln(severity::debug, format, rsl::forward<Args>(args)...);
+        #endif
+    }
+
+    template <typename... Args>
+    void undecorated_info([[maybe_unused]] const format_string format, [[maybe_unused]] Args&&... args)
+    {
+        #if !defined(RSL_STRIP_INFO_LOG)
+        undecoratedln(severity::info, format, rsl::forward<Args>(args)...);
+        #endif
+    }
+
+    template <typename... Args>
+    void undecorated_warn(const format_string format, Args&&... args)
+    {
+        undecoratedln(severity::warn, format, rsl::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    void undecorated_error(const format_string format, Args&&... args)
+    {
+        undecoratedln(severity::error, format, rsl::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    void undecorated_fatal(const format_string format, Args&&... args)
+    {
+        undecoratedln(severity::fatal, format, rsl::forward<Args>(args)...);
+    }
+}
